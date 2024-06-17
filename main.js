0ff4f132cd79decc3029f9259ddcf1593c62b2c1
Julia Goncalves <jugoncalves.ds@gmail.com>
2024-06-17 20:01:33 -0300

    slider fundo

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 let contador = 1;
+const totalSlides = 5;
 
-setInterval(function(){
-    document.getElementById('slide_' + contador).checked = true;
-    contador ++;
-    if(contador > 5) {
+setInterval(function() {
+    nextSlide();
+}, 3000);
+
+function nextSlide() {
+    contador++;
+    if (contador > totalSlides) {
         contador = 1;
     }
-}, 3000)
+    document.getElementById('slide_' + contador).checked = true;
+}
+
+function previousSlide() {
+    contador--;
+    if (contador < 1) {
+        contador = totalSlides;
+    }
+    document.getElementById('slide_' + contador).checked = true;
+}
